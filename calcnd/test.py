07b77722fa5346f3635042e9c4e8e174e85b554c
Michael Caselli <michael.caselli@dentsplysirona.com>
2018-11-26 20:56:12 -0500

    two add'l analyses for Naomi

--- calcnd/test.py
+++ calcnd/test.py
@@ -8,14 +8,25 @@ THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 input = os.path.join(THIS_DIR, "..", "data", "signdata (33).csv")
 outputdir = "~/naomi/output"
-allowed_misses = 3
-allowed_matches = 10
+allowed_misses = 0
+allowed_matches = None
 mirror_neighbors = True
 
-features = ["SignType 2.0", "MinorLocation 2.0", "SecondMinorLocation",
-            "Movement 2.0", "Contact", "SelectedFingers 2.0",
-            "Flexion 2.0", "FlexionChange", "Spread", "SpreadChange"]
+outputname = ["NotHandshapeFeatures", "HandshapeFeatures"]
+features = [["SignType 2.0",
+             "MajorLocation 2.0",
+             "MinorLocation 2.0",
+             "SecondMinorLocation",
+             "Movement 2.0",
+             "Contact"],
+            ["SelectedFingers 2.0",
+             "Flexion 2.0",
+             "FlexionChange",
+             "Spread",
+             "SpreadChange"]
+            ]
 
-cnd.main(input=input, outputdir=outputdir, allowedmisses=allowed_misses,
-         allowedmatches=allowed_matches, deduplicated=not mirror_neighbors,
-         features=features)
+for name, feat in zip(outputname, features):
+    cnd.main(input=input, outputdir=outputdir, outputname=name,
+             allowedmisses=allowed_misses, allowedmatches=allowed_matches,
+             deduplicated=not mirror_neighbors, features=feat)
