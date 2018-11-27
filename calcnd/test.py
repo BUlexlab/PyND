d04f2ad2d9c074515f9bdd7cfafd1afd02356e5a
Michael Caselli <michael.caselli@dentsplysirona.com>
2018-11-26 20:58:23 -0500

    one more analysis, eleven features, one miss

--- calcnd/test.py
+++ calcnd/test.py
@@ -12,21 +12,19 @@ allowed_misses = 0
 allowed_matches = None
 mirror_neighbors = True
 
-outputname = ["NotHandshapeFeatures", "HandshapeFeatures"]
-features = [["SignType 2.0",
-             "MajorLocation 2.0",
-             "MinorLocation 2.0",
-             "SecondMinorLocation",
-             "Movement 2.0",
-             "Contact"],
-            ["SelectedFingers 2.0",
-             "Flexion 2.0",
-             "FlexionChange",
-             "Spread",
-             "SpreadChange"]
-            ]
+outputname = "elevenFeaturesOneMiss"
+features = ["SignType 2.0",
+            "MajorLocation 2.0",
+            "MinorLocation 2.0",
+            "SecondMinorLocation",
+            "Movement 2.0",
+            "Contact",
+            "SelectedFingers 2.0",
+            "Flexion 2.0",
+            "FlexionChange",
+            "Spread",
+            "SpreadChange"]
 
-for name, feat in zip(outputname, features):
-    cnd.main(input=input, outputdir=outputdir, outputname=name,
-             allowedmisses=allowed_misses, allowedmatches=allowed_matches,
-             deduplicated=not mirror_neighbors, features=feat)
+cnd.main(input=input, outputdir=outputdir, outputname=outputname,
+         allowedmisses=allowed_misses, allowedmatches=allowed_matches,
+         deduplicated=not mirror_neighbors, features=features)
