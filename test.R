81ef695f4ce4df693241495b0ee0f555bd01fa18
Michael Caselli <michael.caselli@dentsplysirona.com>
2018-12-04 22:39:17 -0500

    test script from R, including test data

---
+++ test.R
@@ -0,0 +1,28 @@
+library("reticulate")
+library(dplyr)
+use_virtualenv("naomi")
+cnd <- import("calcnd")
+## nd <- import("calcnd.neighborhood_density_calc")
+
+a <- read.csv("~/naomi/data/signdata (33).csv")
+features <- c("SignType.2.0",
+              "MajorLocation.2.0",
+              "MinorLocation.2.0",
+              "SecondMinorLocation",
+              "Movement.2.0",
+              "Contact",
+              "SelectedFingers.2.0",
+              "Flexion.2.0",
+              "FlexionChange",
+              "Spread",
+              "SpreadChange")
+
+## out <- nd$MinimalPairND(sample_n(a, 100), features)
+## out$WriteCSVSs("./output/", "foo")
+
+nbr <- import("calcnd.neighbors")
+my.nbrs <- nbr$Neighbors(sample_n(a, 100), features)
+my.nbrs$WriteCSVs("./output", "foo")
+
+b.nd <- read.csv("./output/foo-nd.csv", stringsAsFactors=FALSE, na.strings=c("NA", ""))
+b.nbrs <- read.csv("./output/foo-neighbors.csv", stringsAsFactors=FALSE, na.strings=c("NA", ""))
