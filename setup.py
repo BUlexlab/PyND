cf6c0915ee7bc89e18a5467a0ef710d2fe235180
Michael Caselli <michael.caselli@dentsplysirona.com>
2018-12-05 12:42:31 -0500

    add pandas requirement to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
 package = 'pynd'
-version = '0.1dev'
+version = '0.0.9000'
 
 setup(name=package,
       version=version,
       packages=find_packages(),
+      install_requires=[
+          'pandas',
+      ],
       description="Calculate sign neighborhood density",
       url='url')
